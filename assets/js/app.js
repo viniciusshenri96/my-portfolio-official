7442cc893125183a56ded68a2e52185457734a42
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-27 16:57:05 -0300

    :iphone: Starting responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 "use script";
 
 const aboutCourse = document.querySelector(".about__course");
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close");
+const headerNav = document.querySelector(".header__nav");
 
 aboutCourse.addEventListener("click", function (e) {
   const clicked = e.target.closest(".about__course-name");
@@ -8,6 +11,21 @@ aboutCourse.addEventListener("click", function (e) {
 
   clicked.nextElementSibling.classList.toggle("active");
 });
+
+const openMenu = function () {
+  headerNav.classList.add("active");
+  closeBtn.classList.add("active");
+  menuBtn.classList.add("active");
+};
+
+const closeMenu = function () {
+  headerNav.classList.remove("active");
+  closeBtn.classList.remove("active");
+  menuBtn.classList.remove("active");
+};
+
+menuBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
 
 // VANTA.NET({
 //   el: "#vanta",
