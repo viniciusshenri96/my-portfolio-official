dcde626ac036bdf103f33735aaf64c8cea9d10fe
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-28 15:38:29 -0300

    :sparkles: feat: Implementing Revealing Elements on Scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,3 +62,26 @@ setInterval(() => {
   min.style.transform = `rotateZ(${mm}deg)`;
   sec.style.transform = `rotateZ(${ss}deg)`;
 });
+
+// Revealing Elements on Scroll
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+
+  section.classList.add("section--hidden");
+});
